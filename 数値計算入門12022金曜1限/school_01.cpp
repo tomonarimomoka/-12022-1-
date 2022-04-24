8df3f73ed3ed4d18b270445dcde773181e518e55
ﾐｸ ﾄﾓﾅﾘ <tomomiku0112@gmail.com>
2022-04-24 09:52:47 +0900

    プロジェクト ファイルを追加します。

---
+++ 数値計算入門12022金曜1限/school_01.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+ int main() {
+	//int a, b, c;
+
+	int a = 2;
+	int b = 3;
+	int c = 4;
+
+	printf("a+b=%d\n", a + b);
+	printf("c-b=%d\n", c - b);
+	printf("b*c=%d\n", b*c);
+	printf("c/a=%d\n", c/a);
+	printf("c%b=%d\n", c%b);
+}
